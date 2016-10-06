e87a9cf71816af45396c5bc2f37f15bfdd449ddf
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-10-06 16:07:02 -0400

    routes made

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/lorem-ipsum', 'p3Controller@index')->name('lorem-ipsum.index');
+Route::get('/lorem-ipsum/create', 'p3Controller@create')->name('lorem-ipsum.create');
+Route::post('/lorem-ipsum', 'p3Controller@store')->name('lorem-ipsum.store');
+Route::get('/lorem-ipsum/{book}', 'p3Controller@show')->name('lorem-ipsum.show');
+Route::get('/lorem-ipsum/{book}/edit', 'p3Controller@edit')->name('lorem-ipsum.edit');
+Route::put('/lorem-ipsum/{book}', 'p3Controller@update')->name('lorem-ipsum.update');
+Route::delete('/lorem-ipsum/{book}', 'p3Controller@destroy')->name('lorem-ipsum.destroy');
