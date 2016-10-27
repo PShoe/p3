80135742bfb3cabe4ea26d5404f7377b32d234b6
Pauline Shoemaker <shoemaker.pauline@gmail.com>
2016-10-27 12:43:41 -0400

    changed button label

--- resources/views/layouts/loremShow.blade.php
+++ resources/views/layouts/loremShow.blade.php
@@ -3,5 +3,5 @@
 @section('content')
 <h2>Look at your lorem!</h2>
 <p>{!! nl2br(e($paragraphs)) !!}</p>
-<a href="lorem/create"><input class='button' type="submit"  value='Generate more lorem'></a>
+<a href="lorem/create"><input class='button' type="submit"  value='Generate more text'></a>
 @endsection
